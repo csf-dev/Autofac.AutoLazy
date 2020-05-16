13d698107eef903c0b6643214c4a1e12816a1cd9
Craig Fowler <craig@csf-dev.com>
2020-05-16 10:46:20 +0100

    WIP #2 - Remove support for Castle.Core 3.2.0
    This is because in all 3.x versions of Castle.Core, the IProxyGenerator
    interface is missing.  The proxy generator class does not implement any
    interfaces.  This would require us to depend on the concretion (bad).
    
    In theory it could be possible to create a separate build of AutoLazy
    to support CC 3.x, using a compiler constant to switch the interface
    for the concrete class in that build config, but there's no call for that
    right now, just to support an obsolete version.

--- Tools/testAllEnvironments.sh
+++ Tools/testAllEnvironments.sh
@@ -1,8 +1,16 @@
-﻿#!/bin/bash
+#!/bin/bash
 
+echo "Testing with Autofac version 4.2.0"
 dotnet test -c Debug_AF42
+
+echo "Testing with Autofac version 4.9.4"
 dotnet test -c Debug_AF49
+
+echo "Testing with Autofac version 5.2.0"
 dotnet test -c Debug_AF52
-dotnet test -c Debug_CC32
+
+echo "Testing with Castle.Core version 4.0.0"
 dotnet test -c Debug_CC40
+
+echo "Testing with Castle.Core version 4.4.1"
 dotnet test -c Debug_CC44
