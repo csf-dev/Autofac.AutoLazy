6f7014115d9d043d2a4298b492cc63439cbf8c15
Craig Fowler <craig@csf-dev.com>
2020-05-16 10:05:18 +0100

    WIP #2 - Add resolver impl

---
+++ Autofac.AutoLazy/AutoLazyResolver.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Autofac.Core;
+
+namespace Autofac.AutoLazy
+{
+    /// <summary>
+    /// A service which uses an Autofac component context (and optionally, parameters)
+    /// to resolve auto-lazy service instances.
+    /// </summary>
+    public class AutoLazyResolver : IResolvesAutoLazyServices
+    {
+        /// <summary>
+        /// Resolves an auto-lazy implementation of the service type
+        /// <typeparamref name="T"/> (which must be an interface).
+        /// </summary>
+        /// <returns>The auto-lazy service implementations.</returns>
+        /// <param name="ctx">An Autofac component context.</param>
+        /// <param name="params">A collection of Autofac parameters.</param>
+        /// <typeparam name="T">The desired service type (must be an interface).</typeparam>
+        public T ResolveAutoLazyService<T>(IComponentContext ctx, IEnumerable<Parameter> @params = null) where T : class
+        {
+            if (ctx == null)
+                throw new ArgumentNullException(nameof(ctx));
+
+            var autoLazyFactory = ctx.Resolve<IGetsAutoLazyServices>();
+            var lazyInstance = ctx.Resolve<Lazy<T>>(@params ?? Enumerable.Empty<Parameter>());
+            return autoLazyFactory.GetAutoLazyService(lazyInstance);
+        }
+    }
+}
---
+++ Autofac.AutoLazy/IResolvesAutoLazyServices.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using Autofac.Core;
+
+namespace Autofac.AutoLazy
+{
+    /// <summary>
+    /// An object which resolves instances of arbitrary service types (provided the type is an interface),
+    /// returning auto-lazy implementations.
+    /// </summary>
+    public interface IResolvesAutoLazyServices
+    {
+        /// <summary>
+        /// Resolves an auto-lazy implementation of the service type
+        /// <typeparamref name="T"/> (which must be an interface).
+        /// </summary>
+        /// <returns>The auto-lazy service implementations.</returns>
+        /// <param name="ctx">An Autofac component context.</param>
+        /// <param name="params">A collection of Autofac parameters.</param>
+        /// <typeparam name="T">The desired service type (must be an interface).</typeparam>
+        T ResolveAutoLazyService<T>(IComponentContext ctx, IEnumerable<Parameter> @params = null) where T : class;
+    }
+}
